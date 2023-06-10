406897198528de1a8758acd728fa31f92967d7cb
weizai1130 <132725448+weizai1130@users.noreply.github.com>
2023-06-10 03:50:20 +0000

    qwe

--- lesson10_8.py
+++ lesson10_8.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import streamlit as st
 
+#下載youbike資料
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
 if response.status_code == 200:
@@ -12,7 +13,7 @@ else:
     print(f"連線失敗:{response.status_code}")
 
 
-
+#將youbike資料轉成dataFrame
 dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
 
 dataFrame.columns = ["站點名稱","車數","可借","行政區","時間","地址","可還","狀態"]
