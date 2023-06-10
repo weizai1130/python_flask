a0c150b4c707229f916d89f81196fbf1295d2c5e
weizai1130 <132725448+weizai1130@users.noreply.github.com>
2023-06-10 07:22:44 +0000

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -8,4 +8,6 @@ with st.sidebar:
     selected_codes = st.multiselect("請選擇股票:",codeSeries,
                                   max_selections=4)
     
-st.write(selected_codes)
+for code in selected_codes:
+    code = code[:4]+'.TW'
+    st.write(code)
