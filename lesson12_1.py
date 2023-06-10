d54239e426cb788ea8d1f5650e9e63d6d39dae1f
weizai1130 <132725448+weizai1130@users.noreply.github.com>
2023-06-10 07:01:09 +0000

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -5,4 +5,7 @@ codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
 codeSeries = codeFrame["code"].astype(str)+codeFrame['name']
 
 with st.sidebar:
-    st.multiselect("請選擇股票:",codeSeries)
+    selected_codes = st.multiselect("請選擇股票:",codeSeries,
+                                  max_selections=4)
+    
+st.write(selected_codes)
