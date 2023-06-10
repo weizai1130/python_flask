8ad348553273ead7ed1b8564cbf336345ef7906f
weizai1130 <132725448+weizai1130@users.noreply.github.com>
2023-06-10 07:52:38 +0000

    modify

--- lesson12_1.py
+++ lesson12_1.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import pandas as pd
+import yfinance as yf
 
 codeFrame = pd.read_csv('codeSearch.csv',usecols=['code','name'])
-codeSeries = codeFrame["code"].astype(str)+codeFrame['name']
+codeSeries = codeFrame['code'].astype(str) + codeFrame['name']
 
 with st.sidebar:
     selected_codes = st.multiselect("請選擇股票:",codeSeries,
-                                  max_selections=4)
+                                    max_selections=4)
     
+@st.cache_data
+def fetch_stock_dataFrame(id):
+    stock_dataFrame = yf.download(id,start='2022-01-01')
+    return stock_dataFrame
+
 for code in selected_codes:
-    code = code[:4]+'.TW'
-    st.write(code)
+    code1 = code[:4]+'.TW'
+    code_stock_dataFrame = fetch_stock_dataFrame(code1)
+    code_stock_dataFrame_sorted = code_stock_dataFrame.sort_index(ascending=False)
+    st.subheader(code)
+    st.dataframe(code_stock_dataFrame_sorted,width=1024)
+    st.divider()
